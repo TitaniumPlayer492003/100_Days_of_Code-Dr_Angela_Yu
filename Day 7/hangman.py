21ee4be1da1a8e11d4ae069630eef2afc9e50059
TitaniumPlayer492003 <avijeetsingh492003@gmail.com>
2024-03-06 17:38:54 +0530

    Day 7 minor changes; Code Review: PENDING

--- Day 7/hangman.py
+++ Day 7/hangman.py
@@ -1,19 +1,21 @@
 # FINAL hangman
+
 import random
+import os
 from utility_files import logo as lg
 from utility_files import stages
-from utility_files import word_list
+import utility_files
 
-chosen_word = random.choice(word_list) # Choosing a word randomly
+chosen_word = random.choice(utility_files.word_list) # Choosing a word randomly
 lives = 6 # Setting the max amount of lives
 
 print(lg) # printing the logo
-# print(f'Pssst, the solution is {chosen_word}.') #Solution, used while debugging
+print(f'Pssst, the solution is {chosen_word}.') #Solution, used while debugging
 
 display = [] #Initializing a list that will contain _ equal to the number of letters
 for letter in chosen_word: #adding _ to the list
     display.append('_')
-print(display)
+print(f"{' '.join(display)}") # Printing display in form of just blanks instead of a list
 
 incorrect_guesses = [] # Initializing a list that will keep a record of all the wrong inputs
 
@@ -26,12 +28,17 @@ while ('_' in display != 0 and lives > 0): #looping while there are _s left and 
         guess = input("Guess a letter: ").lower() # Converting input into lower_case
         if len(guess)>1:
             print('You can only guess one letter.')
+        else: # Clearing the screen after every user_guess, improving UX
+            os.system('cls')
     
     # If guess is correct
     if guess in chosen_word:
-        for index, letter in enumerate(chosen_word):# replacing _ with guessed letter in display
+        if guess in display:
+            print(f"You've already guessed {guess}")
+        for index, letter in enumerate(chosen_word):# replacing _ with guessed letter in display; telling the user the guess is correct
             if letter == guess:
                 display[index] = letter
+                print('Correct guess!')
     
     # If guess is incorrect
     else:
@@ -44,10 +51,9 @@ while ('_' in display != 0 and lives > 0): #looping while there are _s left and 
             lives -= 1
             incorrect_guesses.append(guess)
             print(f"'{guess}' is not in the word.")
-        
-        print(stages[lives]) #NOTE: Lives(in ascii art) are only displayed if a life is lost in that attempt
+            print(stages[lives]) #NOTE: Lives(in ascii art) are only displayed if a life is lost in that attempt
     
-    print(display)
+    print(f"{' '.join(display)}") # Printing display in form of just blanks instead of a list
 
 # Once we exit the loop:
 # Check Win
