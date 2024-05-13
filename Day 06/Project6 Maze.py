57e1486ccabcc9267d9ba171ebe1d5ab42d1223f
TitaniumPlayer492003 <avijeetsingh492003@gmail.com>
2024-05-13 12:22:05 +0530

    Project6 Maze.py : Fixed only remaining bug after completing Day 14

--- Day 06/Project6 Maze.py
+++ Day 06/Project6 Maze.py
@@ -3,11 +3,33 @@ def turn_right():
     turn_left()
     turn_left()
 
+
 while not at_goal():
     if front_is_clear() and not right_is_clear():
         move()
     elif right_is_clear():
         turn_right()
+        move()
+    else:
+        turn_left()
+
+
+# after bug-fix:
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
         move()
     else:
         turn_left()
