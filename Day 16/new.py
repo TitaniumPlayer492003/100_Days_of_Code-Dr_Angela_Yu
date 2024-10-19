e9c6a1442a861bce521764712b44805215c4d508
Avijeet <avijeetsingh492003@gmail.com>
2024-10-19 23:59:38 +0530

    resuming the grind

--- Day 16/new.py
+++ Day 16/new.py
@@ -1,23 +1,2 @@
-import turtle
+from turtle import Turtle, Screen
 
-timmy = turtle.Turtle()
-timmy.shape("turtle")
-timmy.speed('slowest')
-
-
-def is_even(n):
-    return n % 2 == 0
-
-
-for i in range(6):
-    for j in range(360):
-        if is_even(i):
-            timmy.color("violet")
-        else:
-            timmy.color("coral")
-        timmy.forward(1)
-        timmy.left(1)
-    timmy.left(60)
-timmy.color("darkgreen")
-timmy.right(90)
-timmy.forward(300)
